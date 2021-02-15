b63ee891cfe5d9aa191f7a2106f49c236004baf8
Marcin Kordus <marcin.kordus8@gmail.com>
2021-02-15 20:16:48 +0100

    Added mx keyswitch plate gaps

--- src/test/scala/yak/YakTest.scala
+++ src/test/scala/yak/YakTest.scala
@@ -15,15 +15,13 @@ class YakTest extends AnyFlatSpec {
 
     Union(
       keyBorder.moveY(keyWithBorderWidth * 0),
-      keyBorder.moveY(keyWithBorderWidth * 1),
-      keyBorder.moveY(keyWithBorderWidth * 2),
-      keyBorder.moveY(keyWithBorderWidth * 3)
+      keyBorder.moveY(keyWithBorderWidth * 1)
     )
   }
 
   def drawKeyBorder(
       borderWidth: Double = 2.5,
-      wallWidth: Double = 2.0,
+      wallWidth: Double = 3.0,
       keyWidth: Double = 14.0
   ) = {
     val borderCubeWidth = keyWidth + (borderWidth * 2)
@@ -36,20 +34,38 @@ class YakTest extends AnyFlatSpec {
       )
         .moveX(-borderCubeWidth / 2)
         .moveY(-borderCubeWidth / 2)
-        .moveZ(-wallWidth / 2)
 
-    Union(
+    val gapDepth = 1.0
+    val gapWidth = 5.0
+
+    val gap =
+      Cube(
+        width = gapWidth,
+        depth = gapDepth,
+        height = wallWidth - 2.0
+      )
+        .moveX(-gapWidth / 2)
+        .moveY((-keyWidth - gapDepth) / 2)
+
+    val borders = Union(
       borderPart.rotateZ(0),
       borderPart.rotateZ(90),
       borderPart.rotateZ(180),
       borderPart.rotateZ(270)
     )
+
+    val gaps = Union(
+      gap.rotateZ(0),
+      gap.rotateZ(180)
+    )
+
+    Difference(borders, gaps)
   }
 
   private def renderTest(obj: Solid): Unit = {
     val renderingOption = List("$fn=100;")
     val renderer = new backends.OpenSCAD(renderingOption)
-    // renderer.view(obj)
-    renderer.toSTL(obj, "v1.stl")
+    renderer.view(obj)
+    // renderer.toSTL(obj, "v1.stl")
   }
 }
